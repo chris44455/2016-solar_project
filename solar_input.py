9200a7f8584e78cdaaa48771d1333bdcbd7cf2d8
ShuleshkoAndrew <55440786+ShuleshkoAndrew@users.noreply.github.com>
2019-11-19 14:27:16 +0300

    Add files via upload
    Код с многими дополнениями

--- solar_input.py
+++ solar_input.py
@@ -25,6 +25,12 @@ def read_space_objects_data_from_file(input_filename):
                 objects.append(star)
             else:
                 print("Unknown space object")
+            if object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
+            else:
+                print("Unknown space object")
 
     return objects
 
@@ -44,7 +50,7 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
 
-    pass  # FIXME: not done yet
+
 
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
--- solar_model.py
+++ solar_model.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # license: GPLv3
 
-gravitational_constant = 6.67408E-11
+G = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
 
@@ -19,8 +19,11 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        #Считаем изменение силы для каждого момента времени
+        dFx = (body.m*obj.m*G/r**3)*(obj.x-body.x)
+        dFy = (body.m*obj.m*G/r**3)*(obj.y-body.y)
+        body.Fx += dFx  # FIXME: нужно вывести формулу...
+        body.Fy += dFy  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
@@ -32,10 +35,12 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
+    body.x += body.Vx*dt  # FIXME: не понимаю как менять...
     body.Vx += ax*dt
     # FIXME: not done recalculation of y coordinate!
-
+    ay = body.Fy / body.m
+    body.y += body.Vy * dt
+    body.Vy += ay * dt
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
